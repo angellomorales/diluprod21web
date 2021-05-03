a14f4c134d6bfd0925c894280aab2891cfb6c6c9
Angello <ing.angellomorales@gmail.com>
2021-05-02 19:47:31 -0500

    login

--- Basic/views.py
+++ Basic/views.py
@@ -1,6 +1,37 @@
+from django.contrib.auth import authenticate, login, logout
+from django.db import IntegrityError
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.urls import reverse
 
-# Create your views here. 
+from .models import User
+
+
 def index(request):
-    return HttpResponse("Hello, world2!")
+    return render(request, "Basic/index.html")
+
+
+def login_view(request):
+    if request.method == "POST":
+
+        # Attempt to sign user in
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+
+        # Check if authentication successful
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "Basic/login.html", {
+                "message": "Usuario y/o Contraseña invalido."
+            })
+    else:
+        return render(request, "Basic/login.html")
+
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("index"))
+
