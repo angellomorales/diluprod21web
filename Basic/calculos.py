6322c85d489ab00dd80df5c69bcdcf9497520297
Angello <ing.angellomorales@gmail.com>
2021-05-09 22:56:24 -0500

    adicion de init y setvariables

--- Basic/calculos.py
+++ Basic/calculos.py
@@ -1,31 +1,38 @@
 class NewCalculos():
-    def calcularDiluyente(apiCabeza, apiNafta, apiMezclaDeseado, aceite, swCabeza, condicion)
-      apiMezclaObjetivo = apiMezclaDeseado
-       geAceite = 141.5 / (apiCabeza + 131.5)
+    def __init__(self, **kargs):
+        # self.aceite = aceite
+        # self.swCabeza = swCabeza
+        # self.apiCabeza = apiCabeza
+        # self.apiDiluyente = apiDiluyente
+        self.densidadAgua = 62.3774
+
+    def calcularDiluyente(apiCabeza, apiDiluyente, apiMezclaDeseado, aceite, swCabeza, condicion)
+       apiMezclaObjetivo = apiMezclaDeseado
+        geAceite = 141.5 / (apiCabeza + 131.5)
         geAgua = 1
-        geNafta = 141.5 / (apiNafta + 131.5)
+        geDiluyente = 141.5 / (apiDiluyente + 131.5)
 
         agua = (aceite * swCabeza) / (1 - swCabeza)
 
         while condicion
-          geMezclaSeco = 141.5 / (apiMezclaObjetivo + 131.5)
-           nafta = (aceite * (geAceite - geMezclaSeco) + agua *
-                    (geAgua - geMezclaSeco)) / (geMezclaSeco - geNafta)
+           geMezclaSeco = 141.5 / (apiMezclaObjetivo + 131.5)
+            diluyente = (aceite * (geAceite - geMezclaSeco) + agua *
+                     (geAgua - geMezclaSeco)) / (geMezclaSeco - geDiluyente)
 
-            factorC = (nafta / (nafta + aceite)) * 100
-            factorG = apiNafta - apiCabeza
+            factorC = (diluyente / (diluyente + aceite)) * 100
+            factorG = apiDiluyente - apiCabeza
             factorS = 4.86 * pow(10, -8) * factorC * \
-                                 pow(100 - factorC, 0.819) * pow(factorG, 2.28)
+                pow(100 - factorC, 0.819) * pow(factorG, 2.28)
             # factorS=2.14*Math.pow(10, -3)*pow(factorC,-0.0704)*pow(factorG,1.76)
-            encogimiento = (factorS / 100) * (nafta + aceite)
+            encogimiento = (factorS / 100) * (diluyente + aceite)
 
             geMezclaHumedo = (aceite * geAceite + agua * geAgua +
-                              nafta * geNafta) / (aceite + agua + nafta - encogimiento)
-            geMezclaSeco = (aceite * geAceite + nafta * geNafta) / \
-                            (aceite + nafta - encogimiento)
+                              diluyente * geDiluyente) / (aceite + agua + diluyente - encogimiento)
+            geMezclaSeco = (aceite * geAceite + diluyente * geDiluyente) / \
+                (aceite + diluyente - encogimiento)
 
-            swMezcla = agua / (agua + nafta + aceite - encogimiento)
-            # nafta = (aceite * (geAceite - geMezclaSeco) + agua * (geAgua - geMezclaSeco)) / (geMezclaSeco - geNafta)
+            swMezcla = agua / (agua + diluyente + aceite - encogimiento)
+            # diluyente = (aceite * (geAceite - geMezclaSeco) + agua * (geAgua - geMezclaSeco)) / (geMezclaSeco - geDiluyente)
 
             apiMezclaHumedo = (141.5 / geMezclaHumedo) - 131.5
             apiMezclaSeco = (141.5 / geMezclaSeco) - 131.5
@@ -36,10 +43,35 @@ class NewCalculos():
             res = round(apiMezclaSeco, 2) - round(apiMezclaDeseado, 2)
             if (res > 0)
                if (res > 2)
-                  apiMezclaObjetivo = apiMezclaObjetivo - 1
+                   apiMezclaObjetivo = apiMezclaObjetivo - 1
                 else
-                  apiMezclaObjetivo = apiMezclaObjetivo - 0.01
+                   apiMezclaObjetivo = apiMezclaObjetivo - 0.01
             else
-              apiMezclaObjetivo = apiMezclaObjetivo + 0.01
+               apiMezclaObjetivo = apiMezclaObjetivo + 0.01
             if ((res < 0.01 and res > -0.01) or (res < 0.5 and res > -0.5 and swCabeza > 0.9) or (res < 0.1 and res > -0.1 and swCabeza > 0.7))
-              condicion = false
+               condicion = false
+        setVariables(geMezclaSeco, geDiluyente, agua, swMezcla, diluyente, aceite,
+                     geAceite, factorS, apiMezclaHumedo, apiMezclaSeco, gemyLiquido)
+
+    def setVariables(self, **kargs):        
+        self.agua = agua
+        self.diluyente = diluyente
+
+        self.swMezcla = swMezcla
+
+        self.apiMezclaHumedo = apiMezclaHumedo
+        self.apiMezclaSeco = apiMezclaSeco
+
+        self.geCabeza = geCabeza
+        self.geDiluyente = geDiluyente
+        self.geEmulsion = geEmulsion
+        self.geLiquido = geLiquido
+      
+        self.densidadEmulsion = self.densidadAgua*self.geMezclaSeco
+        self.densidadDiluyente = self.densidadAgua*self.geDiluyente
+        self.densidadLiquido = self.densidadAgua*self.geLiquido
+
+        self.relacionOil_Diluyente = diluyente/(aceite+diluyente)
+        self.relacion1_3 = relacion1_3=aceite/3
+
+        self.factorEncogimiento = factorEncogimiento
