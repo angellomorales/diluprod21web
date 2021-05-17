5277dd65b061a4c772f32ed416f0520cdd379ab6
Angello <ing.angellomorales@gmail.com>
2021-05-17 10:29:33 -0500

    cambio de nombre calculos.js

---
+++ Basic/static/Basic/js/calculos.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function () {
+  // alert('funciona');
+  document.querySelector('#id_tipoCalculo_0').addEventListener('click', () => showLabel('Diluyente a inyectar BPD'));// Calcular API
+  document.querySelector('#id_tipoCalculo_1').addEventListener('click', () => showLabel('API Seco @ 60ºF'));//Calcular Diluyente
+
+});
+
+function showLabel(text) {
+  alert('funciona');
+  // document.querySelector('#id_variableACalcular').value = text;
+}
