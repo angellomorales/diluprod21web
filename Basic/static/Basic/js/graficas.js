2d4a15b5aaa90d075945e0ea5dcca3842bfb316a
Angello <ing.angellomorales@gmail.com>
2021-05-17 23:28:18 -0500

    adicion de chart basico

---
+++ Basic/static/Basic/js/graficas.js
@@ -0,0 +1,31 @@
+const labels = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+];
+
+const data = {
+    labels: labels,
+    datasets: [{
+        label: 'Mi primer dataset',
+        backgroundColor: 'rgb(255, 99, 132)',
+        borderColor: 'rgb(255, 99, 132)',
+        data: [0, 10, 5, 2, 20, 30, 45],
+    }]
+};
+
+const config = {
+    type: 'line',
+    data,
+    options: {}
+};
+
+document.addEventListener('DOMContentLoaded', function () {
+    var myChart = new Chart(
+        document.getElementById('chart'),
+        config
+    );
+});
