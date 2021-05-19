5176203f76382f83f6e38260c103b1c948ff3ce3
Angello <ing.angellomorales@gmail.com>
2021-05-18 23:30:58 -0500

    correccion CSS form calculos

--- Basic/forms.py
+++ Basic/forms.py
@@ -14,9 +14,21 @@ class CalculosForm(forms.Form):
     apiDiluyente = forms.DecimalField(
         decimal_places=2, required=True, label="API Diluyente @60ºF")
     tipoCalculo = forms.ChoiceField(
-        choices=CHOICES, required=True, label="Tipo calculo", widget= forms.RadioSelect, initial='apiMezcla')
+        choices=CHOICES, required=True, label="Tipo calculo", widget=forms.RadioSelect, initial='apiMezcla')
     variableACalcular = forms.DecimalField(
         decimal_places=2, required=True, label="Diluyente a inyectar BPD")
 
-    
-
+    def __init__(self, *args, **kwargs):
+        super(CalculosForm, self).__init__(*args, **kwargs)
+        self.fields['pozo'].widget.attrs.update(
+            {'class': 'form-control mx-sm-3 mb-2'})
+        self.fields['aceite'].widget.attrs.update(
+            {'class': 'form-control mx-sm-3 mb-2'})
+        self.fields['swCabeza'].widget.attrs.update(
+            {'class': 'form-control mx-sm-3 mb-2'})
+        self.fields['apiCabeza'].widget.attrs.update(
+            {'class': 'form-control mx-sm-3 mb-2'})
+        self.fields['apiDiluyente'].widget.attrs.update(
+            {'class': 'form-control mx-sm-3 mb-2'})
+        self.fields['variableACalcular'].widget.attrs.update(
+            {'class': 'form-control mx-sm-3 mb-2'})
