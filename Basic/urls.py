138147fd4d15dd0391617b51b25579da7b515872
Angello <ing.angellomorales@gmail.com>
2021-05-25 10:01:51 -0500

    correccion ID para graficas

--- Basic/urls.py
+++ Basic/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
     path("calculos", views.calculos_view, name="calculos"),
-    path("graficas/<str:id>", views.graficas_view, name="graficas"),
+    path("graficas/<str:graphId>", views.graficas_view, name="graficas"),
 ]
