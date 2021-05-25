3aaf6f6ee99af436744a56f1cbfb36bd6d65fdc8
Angello <ing.angellomorales@gmail.com>
2021-05-25 11:54:15 -0500

    adicion funcion retorna datos de grafica en JSON

--- Basic/static/Basic/js/graficas.js
+++ Basic/static/Basic/js/graficas.js
@@ -24,8 +24,64 @@ const config = {
 };
 
 document.addEventListener('DOMContentLoaded', function () {
-    var myChart = new Chart(
-        document.getElementById('chart'),
-        config
-    );
+    if (document.getElementById('relacionDiluyente')) {
+        document.getElementById('relacionDiluyente').addEventListener('click', () => load_data('relacionDiluyente'));
+    }
+    if (document.getElementById('chart')) {
+        var myChart = new Chart(
+            document.getElementById('chart'),
+            config
+        );
+    }
+
+
 });
+
+function load_data(graphId) {
+    const pozo = document.querySelector('#id_pozo').value;
+    const aceite = document.querySelector('#id_aceite').value;
+    const apiCabeza = document.querySelector('#id_apiCabeza').value;
+    const apiDiluyente = document.querySelector('#id_apiDiluyente').value;
+    const apiMezclaHumedo = document.getElementById('id_apiMezclaHumedo').innerHTML;
+    fetch(`/graficas/${graphId}`, {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: {
+            "X-CSRFToken": getCookie("csrftoken")
+        },
+        body: JSON.stringify({
+            pozo: pozo,
+            aceite: aceite,
+            apiCabeza: apiCabeza,
+            apiDiluyente: apiDiluyente,
+            apiMezclaHumedo: apiMezclaHumedo
+        })
+    })
+        .then(response => response.json())
+        // .then(status)
+        .then(dataGraph => {
+            // Print data
+            console.log(`${graphId}:`);
+            console.log(dataGraph);
+
+            // ... do something else with dataGraph ...
+        })
+        .catch(err => console.log(err));
+}
+
+//funcion propia sugerida por django https://docs.djangoproject.com/en/1.11/ref/csrf/#ajax para sacar el csrf de las cookies cuando ya estaba presente
+function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
--- Basic/views.py
+++ Basic/views.py
@@ -1,4 +1,6 @@
-from Basic.calculos import NewCalculos
+import json
+from django.http.response import JsonResponse
+from Basic.calculos import Calculos
 from django.contrib.auth import authenticate, login, logout
 from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
@@ -8,7 +10,7 @@ from django.contrib.auth.decorators import login_required
 
 from .models import User
 from .forms import CalculosForm
-from .calculos import NewCalculos
+from .calculos import Calculos
 
 
 def index(request):
@@ -45,22 +47,24 @@ def calculos_view(request):
     if request.method == "POST":
         form = CalculosForm(request.POST)
         if form.is_valid():
-            pozo=form.cleaned_data["pozo"]
+            pozo = form.cleaned_data["pozo"]
             aceite = form.cleaned_data["aceite"]
             swCabeza = form.cleaned_data["swCabeza"]
             apiCabeza = form.cleaned_data["apiCabeza"]
             apiDiluyente = form.cleaned_data["apiDiluyente"]
             tipoCalculo = form.cleaned_data["tipoCalculo"]
             variableACalcular = form.cleaned_data["variableACalcular"]
-            calculos= NewCalculos()
+            calculos = Calculos()
             if tipoCalculo == 'apiMezcla':
-                calculos.calcularAPI(apiCabeza,apiDiluyente,variableACalcular,aceite,swCabeza)
+                calculos.calcularAPI(
+                    apiCabeza, apiDiluyente, variableACalcular, aceite, swCabeza)
             else:
-                calculos.calcularDiluyente(apiCabeza,apiDiluyente,variableACalcular,aceite,swCabeza,True)
+                calculos.calcularDiluyente(
+                    apiCabeza, apiDiluyente, variableACalcular, aceite, swCabeza, True)
             return render(request, "Basic/calculos.html", {
                 "form": form,
                 "esCalculado": True,
-                "calculos":calculos
+                "calculos": calculos
             })
         else:
             # form.fields['category'].choices = CategoryChoices.choices#add a choices in category
@@ -72,6 +76,28 @@ def calculos_view(request):
         "form": form
     })
 
+
 @login_required(login_url="index")
-def graficas_view(request):
-    return render(request, "Basic/graficas.html")
+def graficas_view(request, graphId):
+    if request.method == "POST":
+        data = json.loads(request.body)
+        calculos = Calculos()
+        pozo = data.get("pozo")
+        aceite = data["aceite"]
+        apiCabeza = data["apiCabeza"]
+        apiDiluyente = data.get("apiDiluyente")
+        variableACalcular = data.get("apiMezclaHumedo")
+        dataGraph={}
+        diluyenteAInyectar = []
+        # diluyenteAInyectar = {}
+        for i in range(99):
+            sw = i*0.01
+            calculos.calcularDiluyente(
+                apiCabeza, apiDiluyente, variableACalcular, aceite, sw, False)
+            diluyenteAInyectar.append(calculos.diluyente)# para list usar safe=false en el jsonresponse
+            # diluyenteAInyectar[i]=calculos.diluyente
+        if graphId == "relacionDiluyente":
+            return JsonResponse(diluyenteAInyectar, safe=False)
+            # return JsonResponse(diluyenteAInyectar)
+
+    return render(request, "Basic/calculos.html")
