e5b19304cbb00233fbb0c0f246a50a2d68695051
Angello <ing.angellomorales@gmail.com>
2021-05-26 01:20:59 -0500

    funciones genericas para manipular n series en la grafica

--- Basic/static/Basic/js/graficas.js
+++ Basic/static/Basic/js/graficas.js
@@ -1,42 +1,11 @@
 // var data = [{ x: 'Sales', series1: 1500, series2:0 }, { x: 'Purchases', series1: 500, series2:0 }, { x: 'Sales2', series1: 1500, series2:0 }]
-var data = []
-
-const config = {
-    type: 'line',
-    data: {
-        datasets: [{
-            label: 'series1 dataset',
-            backgroundColor: 'rgb(255, 99, 132)',
-            borderColor: 'rgb(255, 99, 132)',
-            data: data,
-            parsing: {
-                yAxisKey: 'series1'
-            }
-        }, 
-        //un dataset por cada serie que desee agregar
-        {
-            label: 'series2 dataset',
-            backgroundColor: 'rgb(180, 99, 200)',
-            borderColor: 'rgb(180, 99, 200)',
-            data: data,
-            parsing: {
-                yAxisKey: 'series2'
-            }
-        }]
-    },
-    options: {
-        parsing: {
-            xAxisKey: 'x'
-        }
-    }
-};
 
 document.addEventListener('DOMContentLoaded', function () {
     // listeners para obtener los datos de la grafica
     if (document.getElementById('relacionDiluyente')) {
         document.getElementById('relacionDiluyente').addEventListener('click', () => load_data('relacionDiluyente'));
     }
-    // grafica
+    // grafica default
     load_data('relacionDiluyente');
 
 
@@ -66,11 +35,9 @@ function load_data(graphId) {
         // .then(status)
         .then(dataGraph => {
             // Print data
-            // console.log(`${graphId}:`);
             // console.log(dataGraph);
-            graficar(dataGraph)
+            graficar(dataGraph.datos, dataGraph.params);
 
-            // ... do something else with dataGraph ...
         })
         .catch(err => console.log(err));
 }
@@ -92,18 +59,51 @@ function getCookie(name) {
     return cookieValue;
 }
 
-function graficar(dataValue) {
-    data.splice(0,data.length);
-    dataValue.relacionDiluyente.forEach(element => {
+function graficar(dataValue, params) {
+    var data = [];
+    // data.splice(0, data.length);
+    dataValue.forEach(element => {
         data.push(element);
     });
-    console.log(data);
+    // console.log(data);
     document.querySelector("#chartContent").innerHTML = '<canvas id="chart"></canvas>';
     var chart = document.getElementById('chart').getContext("2d");
     if (chart) {
         var myChart = new Chart(
             document.getElementById('chart'),
-            config
+            configGraph(data, params)
         );
     }
+}
+function configGraph(data, params) {
+    selfDataset = [];
+    for (var key in data[0]) {
+        if (key != 'x') {
+            // console.log(params[key].label);
+            dataset = {
+                label: params[key].label,
+                backgroundColor: params[key].backgroundColor,
+                borderColor: params[key].borderColor,
+                data: data,
+                parsing: {
+                    yAxisKey: key
+                }
+            };
+            selfDataset.push(dataset);
+            // console.log(selfDataset);
+        }
+    }
+    var config = {
+        type: 'line',
+        data: {
+            datasets: selfDataset
+        },
+        options: {
+            parsing: {
+                xAxisKey: 'x'
+            }
+            //configurar titulos y demas
+        }
+    };
+    return config;
 }
