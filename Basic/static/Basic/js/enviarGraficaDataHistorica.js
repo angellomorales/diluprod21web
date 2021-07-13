8a8316e2031626862bc26fb4968a975ca482d1fa
Angello <ing.angellomorales@gmail.com>
2021-07-12 22:49:06 -0500

    recoleccion de datos para enviar a fetch graficas, con las series activas

--- Basic/static/Basic/js/enviarGraficaDataHistorica.js
+++ Basic/static/Basic/js/enviarGraficaDataHistorica.js
@@ -1,40 +1,53 @@
-// // var data = [{ x: 'Sales', series1: 1500, series2:0 }, { x: 'Purchases', series1: 500, series2:0 }, { x: 'Sales2', series1: 1500, series2:0 }]
+var seriesId;
+const seriesActivas = new Set();
 
-// document.addEventListener('DOMContentLoaded', function () {
-//     // listeners para obtener los datos de la grafica
-//     if (document.getElementById('relacionDiluyente')) {
-//         document.getElementById('relacionDiluyente').addEventListener('click', () => load_data('relacionDiluyente'));
-//     }
-//     if (document.getElementById('diluyenteRequerido')) {
-//         document.getElementById('diluyenteRequerido').addEventListener('click', () => load_data('diluyenteRequerido'));
-//     }
-//     if (document.getElementById('limiteRestriccion')) {
-//         document.getElementById('limiteRestriccion').addEventListener('click', () => load_data('limiteRestriccion'));
-//     }
-//     // grafica default
-//     if (document.getElementById('id_apiMezclaHumedo')) {
-//         load_data('relacionDiluyente');
-//     }
+document.addEventListener('click', event => {
+    const element = event.target;
+    if (element.className === 'checkSeriesData') {
+        seriesId.forEach(recorrerSeriesActivas);
+        // console.log('en click');
+        // console.log(seriesActivas);
+        // console.log(seriesActivas.size);
+        // grafica default
+        if (seriesActivas.size > 0) {
+            load_data('historial');
+        }
+    }
+})
 
 
-// });
 
-// function load_data(graphId) {
-//     const pozo = document.querySelector('#id_pozo').value;
-//     const aceite = document.querySelector('#id_aceite').value;
-//     const apiCabeza = document.querySelector('#id_apiCabeza').value;
-//     const apiDiluyente = document.querySelector('#id_apiDiluyente').value;
-//     const apiMezclaHumedo = document.getElementById('id_apiMezclaHumedo').innerHTML;
-//     const idContenedor=document.querySelector('#chartDataHistorica').id;
-//     url = `/graficarDataHistorica/${graphId}`;
-//     bodyJson = JSON.stringify({
-//         pozo: pozo,
-//         aceite: aceite,
-//         apiCabeza: apiCabeza,
-//         apiDiluyente: apiDiluyente,
-//         apiMezclaHumedo: apiMezclaHumedo
-//         idContenedor:idContenedor
-//     });
-//     enviarAJAX(url, bodyJson);
+document.addEventListener('DOMContentLoaded', function () {
+    seriesId = document.querySelectorAll('input.checkSeriesData')
+    // seriesId.forEach(id=>{
+    //     alert(id.id;
+    // });
+    seriesId.forEach(recorrerSeriesActivas);
+    // console.log('inicio');
+    // console.log(seriesActivas);
 
-// }
+});
+
+function recorrerSeriesActivas(serie) {
+    if (serie.checked == true) {
+        seriesActivas.add(serie.id);
+    } else if (seriesActivas.has(serie.id)) {
+        seriesActivas.delete(serie.id);
+    }
+}
+
+function load_data(graphId) {
+    const pozo = document.querySelector('#id_pozo').value;
+    const series=Array.from(seriesActivas);
+    const idContenedor=document.querySelector('#chartDataHistorica').id;
+    url = `/graficarDataHistorica/${graphId}`;
+    bodyJson = JSON.stringify({
+        pozo: pozo,
+        series:series,
+        idContenedor:idContenedor
+    });
+    console.log('json enviado');
+    // console.log(bodyJson);
+    // enviarAJAX(url, bodyJson);
+
+}
