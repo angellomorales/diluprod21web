556238ff7eb9a0a10c6e08f7f91a03fd7d9f594e
Angello <ing.angellomorales@gmail.com>
2021-07-21 07:23:35 -0500

    paso de informacion de la serie en JSON

--- Basic/static/Basic/js/enviarGraficaDataHistorica.js
+++ Basic/static/Basic/js/enviarGraficaDataHistorica.js
@@ -30,39 +30,42 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 function recorrerSeriesActivas(serie) {
-    // let serieparameters = {};//opcion para crear un json
     if (serie.checked == true) {
 
-        //opcion para crear un json
-        // Object.defineProperty(serieparameters, 'id', {
-        //     value: serie.id,
-        // });
+        /*//opcion para crear propiedades a un json
+        let serieparameters={};
+        Object.defineProperty(serieparameters, 'id', {
+            value: serie.id,
+        });
 
-        // Object.defineProperty(serieparameters, 'color', {
-        //     value: document.querySelector(`#${serie.id}_color`).value,
-        // });
-        // Object.defineProperty(serieparameters, 'unidades', {
-        //     value: document.querySelector(`#${serie.id}_unidades`).value,
-        // });
+        Object.defineProperty(serieparameters, 'color', {
+            value: document.querySelector(`#${serie.id}_color`).value,
+        });
+        Object.defineProperty(serieparameters, 'unidades', {
+            value: document.querySelector(`#${serie.id}_unidades`).value,
+        });*/
 
-        seriesActivas.add(serie.id);
+        seriesActivas.add({
+            'id': serie.id,
+            'color': document.querySelector(`#${serie.id}_color`).value,
+            'unidades': document.querySelector(`#${serie.id}_unidades`).value
+        });
 
     }
 }
 
 function load_data(graphId) {
     const pozo = document.querySelector('#id_pozo').value;
-    const series=Array.from(seriesActivas);
-    // let mapIdSeries = series.map((serie) => serie.id);//opcion para crear un json
-    const idContenedor=document.querySelector('#chartDataHistorica').id;
+    const series = Array.from(seriesActivas);
+    const idContenedor = document.querySelector('#chartDataHistorica').id;
     url = `/graficarDataHistorica/${graphId}`;
     bodyJson = JSON.stringify({
         pozo: pozo,
-        series:series,
-        idContenedor:idContenedor
+        series,
+        idContenedor: idContenedor
     });
     // console.log('json enviado');
     // console.log(bodyJson);
-    enviarAJAX(url, bodyJson);
+    enviarAJAX(url, bodyJson);    
 
 }
