91550c07159dfb43c3e1a14d94fd260e485e4aeb
Angello <ing.angellomorales@gmail.com>
2021-08-31 23:43:42 -0500

    correccion error null image

--- Basic/static/Basic/js/app.js
+++ Basic/static/Basic/js/app.js
@@ -4,61 +4,63 @@
 
 var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-toggle="popover"]'))
 var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
-  return new bootstrap.Popover(popoverTriggerEl)
+	return new bootstrap.Popover(popoverTriggerEl)
 })
 
 /* ==== Enable Bootstrap Alert ====== */
 var alertList = document.querySelectorAll('.alert')
 alertList.forEach(function (alert) {
-  new bootstrap.Alert(alert)
+	new bootstrap.Alert(alert)
 });
 
 
 /* ===== Responsive Sidepanel ====== */
-const sidePanelToggler = document.getElementById('sidepanel-toggler'); 
-const sidePanel = document.getElementById('app-sidepanel');  
-const sidePanelDrop = document.getElementById('sidepanel-drop'); 
-const sidePanelClose = document.getElementById('sidepanel-close'); 
-const labImage=document.getElementById('lab-image');
+const sidePanelToggler = document.getElementById('sidepanel-toggler');
+const sidePanel = document.getElementById('app-sidepanel');
+const sidePanelDrop = document.getElementById('sidepanel-drop');
+const sidePanelClose = document.getElementById('sidepanel-close');
+const labImage = document.getElementById('lab-image');
 
-window.addEventListener('load', function(){
-	responsiveSidePanel(); 
+window.addEventListener('load', function () {
+	responsiveSidePanel();
 	responsiveLabImage();
 });
 
-window.addEventListener('resize', function(){
-	responsiveSidePanel(); 
+window.addEventListener('resize', function () {
+	responsiveSidePanel();
 	responsiveLabImage();
 });
 
 
 function responsiveSidePanel() {
-    let w = window.innerWidth;
-	if(w >= 1200) {
-	    // if larger 
-	    //console.log('larger');
+	let w = window.innerWidth;
+	if (w >= 1200) {
+		// if larger 
+		//console.log('larger');
 		sidePanel.classList.remove('sidepanel-hidden');
 		sidePanel.classList.add('sidepanel-visible');
-		
+
 	} else {
-	    // if smaller
-	    //console.log('smaller');
-	    sidePanel.classList.remove('sidepanel-visible');
+		// if smaller
+		//console.log('smaller');
+		sidePanel.classList.remove('sidepanel-visible');
 		sidePanel.classList.add('sidepanel-hidden');
 	}
 };
 
 function responsiveLabImage() {
-    let w = window.innerWidth;
-	if(w >= 768) {
-	    // if larger 
-	    //console.log('larger');
-		labImage.style.display='inline';
-		
-	} else {
-	    // if smaller
-	    //console.log('smaller');
-	    labImage.style.display='none';
+	let w = window.innerWidth;
+	if (!!labImage) {
+		if (w >= 768) {
+			// if larger 
+			//console.log('larger');
+			labImage.style.display = 'inline';
+
+		} else {
+			// if smaller
+			//console.log('smaller');
+			labImage.style.display = 'none';
+		}
 	}
 };
 
@@ -67,7 +69,7 @@ sidePanelToggler.addEventListener('click', () => {
 		console.log('visible');
 		sidePanel.classList.remove('sidepanel-visible');
 		sidePanel.classList.add('sidepanel-hidden');
-		
+
 	} else {
 		console.log('hidden');
 		sidePanel.classList.remove('sidepanel-hidden');
@@ -95,19 +97,19 @@ const searchBox = document.querySelector('.app-search-box');
 searchMobileTrigger.addEventListener('click', () => {
 
 	searchBox.classList.toggle('is-visible');
-	
+
 	let searchMobileTriggerIcon = document.querySelector('.search-mobile-trigger-icon');
-	
-	if(searchMobileTriggerIcon.classList.contains('fa-search')) {
+
+	if (searchMobileTriggerIcon.classList.contains('fa-search')) {
 		searchMobileTriggerIcon.classList.remove('fa-search');
 		searchMobileTriggerIcon.classList.add('fa-times');
 	} else {
 		searchMobileTriggerIcon.classList.remove('fa-times');
 		searchMobileTriggerIcon.classList.add('fa-search');
 	}
-	
-		
-	
+
+
+
 });
 
 
