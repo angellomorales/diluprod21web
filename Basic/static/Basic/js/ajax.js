2522acae2e5dfd30792ad158f14eb9db588bb7e2
Angello <ing.angellomorales@gmail.com>
2021-11-02 22:21:47 -0500

    refactorizacion metodos ajax

---
+++ Basic/static/Basic/js/ajax.js
@@ -0,0 +1,36 @@
+function enviarAJAX(url, bodyJson) {
+    fetch(url, {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: {
+            "X-CSRFToken": getCookie("csrftoken")
+        },
+        body: bodyJson
+    })
+        .then(response => response.json())
+        // .then(status)
+        .then(dataGraph => {
+            // Print data
+            // console.log(dataGraph);
+            graficar(dataGraph);
+
+        })
+        .catch(err => console.log(err));
+}
+
+//funcion propia sugerida por django https://docs.djangoproject.com/en/1.11/ref/csrf/#ajax para sacar el csrf de las cookies cuando ya estaba presente
+function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
