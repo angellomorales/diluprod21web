9b2766ed5911bfa8b0d74c9970348aea7e65d5e2
Angello <ing.angellomorales@gmail.com>
2021-11-19 10:52:51 -0500

    correccion error tabla al calcular laboratorio

--- Basic/static/Basic/js/enviarEventosLaboratorio.js
+++ Basic/static/Basic/js/enviarEventosLaboratorio.js
@@ -1,22 +1,13 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // listeners para obtener los datos de la grafica
-    // if (document.getElementById('relacionDiluyente')) {
-    //     document.getElementById('relacionDiluyente').addEventListener('click', () =>load_data_Graficas('relacionDiluyente'));
-    // }
     const selectPozo = document.querySelector('#id_pozo');
     selectPozo.addEventListener('change', (event) => {
-        load_predata_Calculos(`${event.target.value}`);
+        load_predata_Laboratorio(`${event.target.value}`);
     });
 
-    // grafica default
-    // if (document.getElementById('id_apiMezclaHumedo')) {
-    //     load_data_Graficas('relacionDiluyente');
-    //     const selectElement = document.querySelector('#selectCalculos');
-
-    //     selectElement.addEventListener('change', (event) => {
-    //         load_data_Graficas(`${event.target.value}`);
-    //     });
-    // }
+    // datos x default
+    if (selectPozo.value != "") {
+        load_predata_Laboratorio(selectPozo.value);
+    }
 
 });
 
@@ -39,9 +30,11 @@ document.addEventListener('DOMContentLoaded', function () {
 
 // }
 
-function load_predata_Calculos(PozoId) {
-    url = `/cargarTablaLaboratorio/${PozoId}`;
-    enviarAJAX(url, "");
+function load_predata_Laboratorio(PozoId) {
+    if (PozoId != "") {
+        url = `/cargarTablaLaboratorio/${PozoId}`;
+        enviarAJAX(url, "");
+    }
 }
 
 function cargarPredata(dataResponse) {
@@ -51,8 +44,8 @@ function cargarPredata(dataResponse) {
     }
     else {
 
-        var values = valuesTableHTML(dataResponse);
-        var innerHTML = '<hr class="my-4">' +
+        let values = valuesTableHTML(dataResponse);
+        let innerHTML = '<hr class="my-4">' +
             '<div class="row g-4 settings-section mb-4">' +
             '<h3 class="section-title">Histórico</h3>' +
             '</div>' +
@@ -81,15 +74,15 @@ function cargarPredata(dataResponse) {
     };
 
     function valuesTableHTML(dataResponse) {
-        var header = "";
-        var innerContent = "";
-        var content = "";
+        let header = "";
+        let innerContent = "";
+        let content = "";
 
-        console.log(dataResponse);
+        // console.log(dataResponse);
         dataResponse.datos.forEach(foreachElement);
 
         function foreachElement(element, index, array) {
-            console.log(element);
+            // console.log(element);
             // console.log(index);
             // console.log(arr);
 
@@ -97,16 +90,18 @@ function cargarPredata(dataResponse) {
                 // console.log(field);
                 // console.log(element[field].id);
                 if (index === 0) {
-                    header += `<th class="cell"><span>${field}</span><span class="note">` +
-                        '<div class="form-check form-switch mb-3">' +
-                        `<input class="form-check-input" type="checkbox" id="${element[field].id}">` +
-                        `<label class="form-check-label" for="${element[field].id}">` +
-                        '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-graph-up" viewBox="0 0 16 16">' +
-                        '<path fill-rule="evenodd" d="M0 0h1v15h15v1H0V0zm10 3.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-1 0V4.9l-3.613 4.417a.5.5 0 0 1-.74.037L7.06 6.767l-3.656 5.027a.5.5 0 0 1-.808-.588l4-5.5a.5.5 0 0 1 .758-.06l2.609 2.61L13.445 4H10.5a.5.5 0 0 1-.5-.5z" />' +
-                        '</svg>' +
-                        '</label>' +
-                        '</div>' +
-                        '</span></th>';
+                    header += `<th class="cell"><span>${field}</span><span class="note">`;
+                    if ('color' in element[field]) {
+                        header += '<div class="form-check form-switch mb-3">' +
+                            `<input class="form-check-input" type="checkbox" id="${element[field].id}">` +
+                            `<label class="form-check-label" for="${element[field].id}">` +
+                            '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-graph-up" viewBox="0 0 16 16">' +
+                            '<path fill-rule="evenodd" d="M0 0h1v15h15v1H0V0zm10 3.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-1 0V4.9l-3.613 4.417a.5.5 0 0 1-.74.037L7.06 6.767l-3.656 5.027a.5.5 0 0 1-.808-.588l4-5.5a.5.5 0 0 1 .758-.06l2.609 2.61L13.445 4H10.5a.5.5 0 0 1-.5-.5z" />' +
+                            '</svg>' +
+                            '</label>' +
+                            '</div>';
+                    };
+                    header += '</span></th>';
                 }
                 innerContent += `<td class="cell">${element[field].valor}</td>`;
             }
